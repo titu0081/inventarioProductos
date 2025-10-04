1fe05df672692764f1df6ae8757fe44af3ced88e
titu0081 <101371992+titu0081@users.noreply.github.com>
2025-10-04 02:54:55 -0500

    Cambios realizados en el frontend, para el correcto manejo de transacciones

--- frontend/src/app/componentes/productos/productos.ts
+++ frontend/src/app/componentes/productos/productos.ts
@@ -6,6 +6,7 @@ import {
   ValidationErrors,
   FormGroup,
   ReactiveFormsModule,
+  FormsModule,
 } from '@angular/forms';
 import { InventarioServicio } from '../../servicios/inventarioServicios';
 import Swal from 'sweetalert2';
@@ -21,14 +22,30 @@ export interface Producto {
   stock: number;
 }
 
+interface ProductoConCantidad extends Producto {
+  valorEntero?: number;
+}
+
+export interface Transaccion {
+  id?: number;
+  fecha: string;
+  tipoTransaccion: string;
+  idProducto: number | undefined;
+  cantidad: number | undefined;
+  precioUnitario: number;
+  precioTotal: number;
+  detalle: string;
+}
+
 @Component({
   selector: 'app-productos',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './productos.html',
   styleUrl: './productos.scss',
 })
 export class Productos implements OnInit {
   productos: Producto[] = [];
+  transaccionesPorProductos: Transaccion[] = [];
   tituloError: string = 'Ocurrió un error';
   nombreLongitud: number = 30;
   descripcionLongitud: number = 50;
@@ -37,9 +54,12 @@ export class Productos implements OnInit {
   formulario: FormGroup;
   imagenBase64: string = '';
   mostrarFormularioProducto: boolean = false;
+  mostrarTransaccionesProducto: boolean = false;
   numeroProductosPagina: number = 8;
   paginaActual: number = 1;
   esEditar: boolean = false;
+  valorEntero: number = 0;
+  productoSeleccionado: Producto[] = [];
 
   constructor(private inventarioServicio: InventarioServicio, private formularioProd: FormBuilder) {
     this.formulario = this.formularioProd.group({
@@ -82,13 +102,25 @@ export class Productos implements OnInit {
   get stock() {
     return this.formulario.get('stock');
   }
+
   get totalPaginas(): number {
-    return Math.ceil(this.productos.length / this.numeroProductosPagina);
+    const lista = this.mostrarTransaccionesProducto
+      ? this.transaccionesPorProductos
+      : this.productos;
+    return Math.ceil(lista.length / this.numeroProductosPagina);
   }
 
-  get productosPaginados(): Producto[] {
+  get productosPaginados(): ProductoConCantidad[] {
     const inicio = (this.paginaActual - 1) * this.numeroProductosPagina;
-    return this.productos.slice(inicio, inicio + this.numeroProductosPagina);
+    return this.productos.slice(
+      inicio,
+      inicio + this.numeroProductosPagina
+    ) as ProductoConCantidad[];
+  }
+
+  get transaccionesPaginadas(): Transaccion[] {
+    const inicio = (this.paginaActual - 1) * this.numeroProductosPagina;
+    return this.transaccionesPorProductos.slice(inicio, inicio + this.numeroProductosPagina);
   }
 
   irPagina(num: number) {
@@ -115,10 +147,42 @@ export class Productos implements OnInit {
 
   obtenerTodosProductos() {
     try {
-      this.inventarioServicio.getAllProductos().subscribe({
+      this.inventarioServicio.obtenerTodosProductos().subscribe({
         next: (data: any) => {
           if (data.codigo == 0) {
             this.productos = data.data;
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: this.tituloError,
+              text: data.error,
+            });
+          }
+        },
+        error: (error) => {
+          Swal.fire({
+            icon: 'error',
+            title: this.tituloError,
+            text: 'No se pudo consultar los productos',
+          });
+        },
+      });
+    } catch (error: any) {
+      Swal.fire({
+        icon: 'error',
+        title: this.tituloError,
+        text: error.message,
+      });
+    }
+  }
+
+  obtenerProducto(idProducto: number) {
+    try {
+      this.inventarioServicio.obtenerProducto(idProducto).subscribe({
+        next: (data: any) => {
+          if (data.codigo == 0) {
+            this.productoSeleccionado = [data.data];
+            console.log('ProductoSS', this.productoSeleccionado);
           } else {
             Swal.fire({
               icon: 'error',
@@ -190,7 +254,7 @@ export class Productos implements OnInit {
 
     try {
       if (this.esEditar == false) {
-        this.inventarioServicio.addProducto(datosProducto).subscribe({
+        this.inventarioServicio.nuevoProducto(datosProducto).subscribe({
           next: (data: any) => {
             if (data.codigo == 0) {
               Swal.fire({
@@ -238,7 +302,7 @@ export class Productos implements OnInit {
           precio: nuevoProducto.precio,
           stock: nuevoProducto.stock,
         };
-        this.inventarioServicio.updateProducto(nuevoProducto.id, datosProducto).subscribe({
+        this.inventarioServicio.actualizarProducto(nuevoProducto.id, datosProducto).subscribe({
           next: (data: any) => {
             if (data.codigo == 0) {
               Swal.fire({
@@ -293,6 +357,7 @@ export class Productos implements OnInit {
   regresarProductos() {
     this.reiniciarFormulario();
     this.mostrarFormularioProducto = false;
+    this.mostrarTransaccionesProducto = false;
     this.obtenerTodosProductos();
   }
 
@@ -338,7 +403,7 @@ export class Productos implements OnInit {
     }).then((result) => {
       if (result.isConfirmed) {
         try {
-          this.inventarioServicio.deleteProducto(productoEliminar.id!).subscribe({
+          this.inventarioServicio.eliminarProducto(productoEliminar.id!).subscribe({
             next: (data: any) => {
               if (data.codigo == 0) {
                 Swal.fire({
@@ -377,5 +442,135 @@ export class Productos implements OnInit {
         }
       }
     });
+  }
+
+  agregarTransaccion(producto: Producto, tipoTransaccion: string) {
+    const fechaActual = this.inventarioServicio.retornarFechaFormateada();
+    console.log('Fecha: ', fechaActual, producto);
+    let detalle = '';
+    if (tipoTransaccion == 'compra') {
+      detalle = `Compra éxitosa de: ${producto.nombre}`;
+    } else {
+      detalle = `Venta éxitosa de: ${producto.nombre}`;
+    }
+    let cantidadSeleccionada = (producto as ProductoConCantidad).valorEntero || 0;
+    if (cantidadSeleccionada != 0) {
+      const datosTransaccion: Transaccion = {
+        fecha: fechaActual,
+        tipoTransaccion: tipoTransaccion == 'compra' ? 'compra' : 'venta',
+        idProducto: producto.id,
+        cantidad: cantidadSeleccionada,
+        precioUnitario: producto.precio,
+        precioTotal: cantidadSeleccionada * producto.precio,
+        detalle: detalle,
+      };
+      try {
+        this.inventarioServicio.nuevaTransaccion(datosTransaccion).subscribe({
+          next: (data: any) => {
+            if (data.codigo == 0) {
+              Swal.fire({
+                title: '',
+                text: 'Se ha realizado la transacción con éxito',
+                icon: 'success',
+                buttonsStyling: false,
+                customClass: {
+                  confirmButton: 'btn btn-primary',
+                },
+              }).then(() => {
+                this.regresarProductos();
+              });
+            } else {
+              Swal.fire({
+                icon: 'error',
+                title: this.tituloError,
+                text: data.error,
+                buttonsStyling: false,
+                customClass: {
+                  confirmButton: 'btn btn-primary',
+                },
+              });
+            }
+          },
+          error: (error) => {
+            Swal.fire({
+              icon: 'error',
+              title: this.tituloError,
+              text: 'No se pudo realizar la transacción',
+              buttonsStyling: false,
+              customClass: {
+                confirmButton: 'btn btn-primary',
+              },
+            });
+          },
+        });
+      } catch (error: any) {
+        Swal.fire({
+          icon: 'error',
+          title: this.tituloError,
+          text: error.message,
+        });
+      }
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Revise el valor colocado',
+        text: 'Ingrese un valor mayor a 0',
+        buttonsStyling: false,
+        customClass: {
+          confirmButton: 'btn btn-primary',
+        },
+      });
+    }
+  }
+
+  verTransacciones(producto: Producto) {
+    this.mostrarTransaccionesProducto = false;
+    try {
+      this.inventarioServicio.obtenerTransaccionesPorProducto(producto.id!).subscribe({
+        next: (data: any) => {
+          if (data.codigo == 0 && data.data != '') {
+            this.obtenerProducto(producto.id!);
+            this.transaccionesPorProductos = data.data;
+            this.mostrarTransaccionesProducto = true;
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'No hay transacciones',
+              text: data.error,
+            });
+          }
+        },
+        error: (error) => {
+          Swal.fire({
+            icon: 'error',
+            title: this.tituloError,
+            text: 'No se pudo consultar las transacciones',
+          });
+        },
+      });
+    } catch (error: any) {
+      Swal.fire({
+        icon: 'error',
+        title: this.tituloError,
+        text: error.message,
+      });
+    }
+  }
+
+  filtrar(event: any) {
+    // const search = event.target.value.toLowerCase();
+    // if (search === '') {
+    //   this.InformacionTransaccion = this.TransaccionCompleto;
+    // } else {
+    //   const result = this.TransaccionCompleto.filter((item) => {
+    //     for (const key in item) {
+    //       if (String(item[key]).toLowerCase().indexOf(search) !== -1) {
+    //         return true;
+    //       }
+    //     }
+    //     return false;
+    //   });
+    //   this.InformacionTransaccion = result;
+    // }
   }
 }
--- frontend/src/app/servicios/inventarioServicios.ts
+++ frontend/src/app/servicios/inventarioServicios.ts
@@ -1,32 +1,64 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, LOCALE_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environment/environment';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InventarioServicio {
-  private apiUrl = `${environment.urlProductos}api/Productos`;
+  private apiUrlProducto = `${environment.urlProductos}api/Productos`;
+  private apiUrlTransacciones = `${environment.urlProductos}api/Transacciones`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, @Inject(LOCALE_ID) private locale: string) {}
 
-  getAllProductos() {
-    return this.http.get(this.apiUrl);
+  obtenerTodosProductos() {
+    return this.http.get(this.apiUrlProducto);
   }
 
-  getProducto(id: number) {
-    return this.http.get(`${this.apiUrl}/${id}`);
+  obtenerProducto(id: number) {
+    return this.http.get(`${this.apiUrlProducto}/${id}`);
   }
 
-  addProducto(producto: any) {
-    return this.http.post(this.apiUrl, producto);
+  nuevoProducto(producto: any) {
+    return this.http.post(this.apiUrlProducto, producto);
   }
 
-  updateProducto(id: number, producto: any) {
-    return this.http.put(`${this.apiUrl}/${id}`, producto);
+  actualizarProducto(id: number, producto: any) {
+    return this.http.put(`${this.apiUrlProducto}/${id}`, producto);
   }
 
-  deleteProducto(id: number) {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+  eliminarProducto(id: number) {
+    return this.http.delete(`${this.apiUrlProducto}/${id}`);
+  }
+
+  //Servicios Transacciones
+  obtenerTodasTransacciones() {
+    return this.http.get(this.apiUrlTransacciones);
+  }
+
+  obtenerTransacciones(id: number) {
+    return this.http.get(`${this.apiUrlTransacciones}/${id}`);
+  }
+
+  nuevaTransaccion(producto: any) {
+    return this.http.post(this.apiUrlTransacciones, producto);
+  }
+
+  actualizarTransacciones(id: number, producto: any) {
+    return this.http.put(`${this.apiUrlTransacciones}/${id}`, producto);
+  }
+
+  eliminarTransacciones(id: number) {
+    return this.http.delete(`${this.apiUrlTransacciones}/${id}`);
+  }
+
+  obtenerTransaccionesPorProducto(id: number) {
+    return this.http.get(`${this.apiUrlTransacciones}/PorProducto/${id}`);
+  }
+
+  //Retorno Fecha
+  retornarFechaFormateada(): string {
+    return formatDate(new Date(), 'yyyy-MM-ddTHH:mm:ss', this.locale);
   }
 }
